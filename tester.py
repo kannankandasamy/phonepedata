def3df5d5f0436b00a5b29355cca9dfd2ccb091e
kannan Kandasamy <kannanvijay@hotmail.com>
2023-11-25 12:00:10 -0500

    added all load functions

--- tester.py
+++ tester.py
@@ -67,9 +67,50 @@ class PhonepeAnalytics:
 
         df = dl.get_map_users()
         print(df.size)
+        drop_table_query = "drop table if exists map_users"
+        create_table_query = """
+            create table if not exists map_users(
+                states varchar(500),
+                years varchar(20),
+                quarters varchar(10),
+                districts varchar(500),
+                registered_users bigint,
+                app_opens bigint
+                )
+            """
+        op = mys.load_data_to_mysql_alchemy(df = df, drop_table_query=drop_table_query, create_table_query=create_table_query, table_name="map_users")
+        print(df.size)
+        print(op)    
 
         df = dl.get_top_trans()
         print(df.size)
+        drop_table_query = "drop table if exists top_trans"
+        create_table_query = """
+            create table if not exists top_trans(
+                states varchar(500),
+                years varchar(20),
+                quarters varchar(10),
+                districts varchar(500),
+                transaction_count bigint,
+                transaction_amount float
+                )
+            """
+        op = mys.load_data_to_mysql_alchemy(df = df, drop_table_query=drop_table_query, create_table_query=create_table_query, table_name="top_trans")
+        print(df.size)
+        print(op)          
 
         df = dl.get_top_users()
         print(df.size)
+        drop_table_query = "drop table if exists top_users"
+        create_table_query = """
+            create table if not exists top_users(
+                states varchar(500),
+                years varchar(20),
+                quarters varchar(10),
+                districts varchar(500),
+                registered_users bigint
+                )
+            """
+        op = mys.load_data_to_mysql_alchemy(df = df, drop_table_query=drop_table_query, create_table_query=create_table_query, table_name="top_users")
+        print(df.size)
+        print(op)    
