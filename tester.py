7c2c5d19007c0bd8f27bec4600e3bdac4a4f72a0
kannan Kandasamy <kannanvijay@hotmail.com>
2023-11-24 21:50:51 -0500

    added sql loading functions

--- tester.py
+++ tester.py
@@ -15,13 +15,55 @@ class PhonepeAnalytics:
 
         dl = DataLoader()
         df = dl.get_agg_trans()
+        drop_table_query = "drop table if exists agg_trans"
+        create_table_query = """
+            create table if not exists agg_trans(
+                states varchar(500),
+                years varchar(20),
+                quarters varchar(10),
+                transaction_name varchar(500),
+                transaction_count bigint,
+                transaction_amount float
+                )
+            """
+        op = mys.load_data_to_mysql_alchemy(df = df, drop_table_query=drop_table_query, create_table_query=create_table_query, table_name="agg_trans")
         print(df.size)
+        print(op)
+
 
         df = dl.get_agg_users()
         print(df.size)
+        drop_table_query = "drop table if exists agg_users"
+        create_table_query = """
+            create table if not exists agg_users(
+                states varchar(500),
+                years varchar(20),
+                quarters varchar(10),
+                brands varchar(500),
+                transaction_count bigint,
+                percentage float
+                )
+            """
+        op = mys.load_data_to_mysql_alchemy(df = df, drop_table_query=drop_table_query, create_table_query=create_table_query, table_name="agg_users")
+        print(df.size)
+        print(op)        
 
         df = dl.get_map_trans()
         print(df.size)
+        drop_table_query = "drop table if exists map_trans"
+        create_table_query = """
+            create table if not exists map_trans(
+                states varchar(500),
+                years varchar(20),
+                quarters varchar(10),
+                districts varchar(500),
+                transaction_count bigint,
+                transaction_amount float
+                )
+            """
+        op = mys.load_data_to_mysql_alchemy(df = df, drop_table_query=drop_table_query, create_table_query=create_table_query, table_name="map_trans")
+        print(df.size)
+        print(op)             
 
         df = dl.get_map_users()
         print(df.size)
